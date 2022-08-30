daec687e35ce084a0e045dc13619f5f9533ec6ea
Carlos Cimino <ciminoce@yahoo.com.ar>
2022-08-30 19:51:35 -0300

    Agregar archivos de proyecto.

---
+++ ConsoleAppEjercicio47/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleAppEjercicio47
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese un número entero:");
+            var nroIngresado = int.Parse(Console.ReadLine());
+            for (int contador = 1; contador <= 12; contador++)
+            {
+                var nroMultiplicado = ObtenerMultiplo(nroIngresado, contador);
+                Console.WriteLine($"{nroIngresado}x{contador}={nroMultiplicado}");
+            }
+
+            Console.ReadLine();
+        }
+
+        private static int ObtenerMultiplo(int nro, int multiplicador) => nro * multiplicador;
+        //{
+        //    return nro * multiplicador;
+        //}
+    }
+}
